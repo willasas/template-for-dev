7635ea23224ee78861f43096b35af3f1dc3b8a34
Steven will <33866172+willasas@users.noreply.github.com>
2024-09-05 17:17:00 +0800

    Update App.vue
    add temp for virtuallist and virtualscroll

--- fe-vite-vue3-temp/src/App.vue
+++ fe-vite-vue3-temp/src/App.vue
@@ -2,6 +2,8 @@
 import Header from './components/Header/Header.vue'
 import Popup from './components/Popup/Popup.vue';
 import ImageComponent from './components/Imagecontainer/Imagecontainer.vue';
+import VirtualScroll from './components/VirtualRander/VirtualScroll.vue';
+import VirtualList from './components/VirtualRander/VirtualList.vue';
 import SelectLang from './components/SelectLang/SelectLang.vue'
 import { ref } from 'vue';
 
@@ -15,6 +17,40 @@ let showTextPopupFlag = ref(false);
 function showTextPopup() {
   showTextPopupFlag.value = true;
 }
+
+// VirtualList data
+const listData = ref([
+  { id: 1, value: 'Item 1' },
+  { id: 2, value: 'Item 2' },
+  { id: 3, value: 'Item 3' },
+  { id: 4, value: 'Item 4' },
+  { id: 5, value: 'Item 5' },
+  { id: 6, value: 'Item 6' },
+  { id: 7, value: 'Item 7' },
+  { id: 8, value: 'Item 8' },
+  { id: 9, value: 'Item 9' },
+  { id: 10, value: 'Item 10' },
+  { id: 11, value: 'Item 11' },
+  { id: 12, value: 'Item 12' },
+  { id: 13, value: 'Item 13' },
+  { id: 14, value: 'Item 14' },
+  { id: 15, value: 'Item 15' },
+  { id: 16, value: 'Item 16' },
+  { id: 17, value: 'Item 17' },
+  { id: 18, value: 'Item 18' },
+  { id: 19, value: 'Item 19' },
+  { id: 20, value: 'Item 20' },
+  { id: 21, value: 'Item 21' },
+  { id: 22, value: 'Item 22' },
+  { id: 23, value: 'Item 23' },
+  { id: 24, value: 'Item 24' },
+  { id: 25, value: 'Item 25' },
+  { id: 26, value: 'Item 26' },
+  { id: 27, value: 'Item 27' },
+  { id: 28, value: 'Item 28' },
+  { id: 29, value: 'Item 29' },
+  { id: 30, value: 'Item 30' },
+]);
 </script>
 
 <template>
@@ -24,9 +60,17 @@ function showTextPopup() {
     <div class="hell">{{ $t('setting') }}</div>
     <div class="test">{{ $t('name') }}</div>
 
-    <Popup v-if="showTextPopupFlag" @closed="showTextPopupFlag = false">
-      <p>This is some text content for the popup.</p>
-      <p>You can add any valid Vue template here.</p>
+    <VirtualScroll />
+    <VirtualList :list-data="listData" :item-size="50" />
+
+    <!-- custom popup -->
+    <button @click="openDialog">Open Dialog</button>
+    <Popup v-model="dialogVisible" title="Custom Dialog Title">
+      <p>This is some text content for the dialog.</p>
+      <template #footer>
+        <button @click="dialogVisible = false">Cancel</button>
+        <button type="primary" @click="confirmDialog">Confirm</button>
+      </template>
     </Popup>
 
     <ImageComponent
@@ -35,9 +79,6 @@ function showTextPopup() {
         :lazyLoad="true"
         :placeholder="loadingText"
     />
-    
-    <!-- 弹窗使用 -->
-    <button @click="showTextPopup">Show Text Popup</button>
   </div>
 </template>
 
